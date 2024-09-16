51352ea08ac57a6224db3ea6c9e2c3dd1e2e3b9e
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-09-16 14:15:38 +0000

    Save Work

--- PP1_2.py
+++ PP1_2.py
@@ -4,17 +4,46 @@
 
 def q1():
 
+    name = "Alex"
+    print(name)
+
 def q2():
 
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    print(num1 + num2 + num3)
+
 def q3():
+    
+    num1 = 21
+    num2 = 4
+    print(num1 // num2)
 
 def q4():
 
+    num1 = 6.3
+    num2 = 3.0
+    print(num1 / num2)
+
 def q5():
+
+    msg = "Hello World"
+    print(msg)
+    msg = "Goodbye World"
+    print(msg)
 
 def q6():
 
+    boolean = True
+    print(boolean)
+
 def q7():
+
+    num1 = 7
+    num2 = 2
+    print(num1 % num2)
+
 
 q1()
 q2()
